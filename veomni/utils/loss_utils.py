[task] fix: Exclude padding batches from gradient updates (#455)

veomni/utils/loss_utils.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from typing import Union
 
 import torch
@@ -13,8 +12,15 @@ def count_loss_token(batches: Union[list[dict[str, torch.Tensor]], dict[str, tor
     """Calculate the total number of text_tokens/image_tokens/** for loss in a global batch, or one micro batch."""
     if isinstance(batches, dict):
         batches = [batches]
-    token_len = defaultdict(int)
+    token_len = {
+        "foundation_tokens": torch.tensor(0),
+        "image_decoder_tokens": torch.tensor(0),
+    }
     for batch in batches:
+        # DynamicBatchSizeDataLoader adds padding batches when data is exhausted and drop_last=False
+        # Padding batch tokens should not be counted
+        if batch.get("padding_flag", False):
+            continue
         token_len["foundation_tokens"] += torch.sum(batch["labels"] != IGNORE_INDEX)  # text tokens
         if "image_output_mask" in batch:
             token_len["image_decoder_tokens"] += torch.sum(batch["image_output_mask"])  # image generation tokens